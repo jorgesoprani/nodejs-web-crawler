99efa178091c6cdeb3713c6d5c6fb22b24ca0b2e
Jorge Soprani <jorgesoprani@gmail.com>
2021-03-14 19:31:19 -0300

    Tested with americanas

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const puppeteer = require("puppeteer");
+const fs = require('fs');
 
 (async () => {
   const searchUrl = "https://www.americanas.com.br/busca/@search";
   const searchTerm = "batata";
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
+
   const page = await browser.newPage();
+  await page.setUserAgent(`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36`);
+
+  // await page.setViewport({
+  //   height: 768,
+  //   width: 1366
+  // });
   const finalUrl = searchUrl.replace("@search", encodeURIComponent(searchTerm));
   console.log(finalUrl);
   await page.goto(finalUrl, {
     waitUntil: "networkidle0",
   });
+
+  await page.evaluate(() => document.querySelector('#lgpd-accept')?.click());
 
   const options = {
     selectors: {
@@ -29,41 +39,61 @@ const puppeteer = require("puppeteer");
             selector: '[class*="src__Wrapper"] > a',
             propName: "href",
           },
-          link: {
-            selector: '[class*="src__PromotionalPrice"] > a',
+          price: {
+            selector: 'span[class*="src__PromotionalPrice"]',
             propName: "textContent",
           },
         },
       },
     },
   };
+  const pageData = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+  fs.writeFile('C:\\Temp\\page.html', pageData, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
 
   const result = await page.evaluate((options) => {
     const allResults = [];
-    for (const selector of Object.keys(options.selectors)) {
-      const nodeList = [
-        ...document.querySelectorAll(options.selectors[selector].parent),
-      ];
-      return document.getElementsByTagName('body')[0].innerHTML;
+    try {
 
-      //   return options.selectors[selector].parent;
-      const result = [];
-      for (const node of nodeList) {
-        const obj = {};
-        const props = options.selectors[selector].props;
-        for (const prop of Object.keys(props)) {
-          const propNode = node.querySelector(props[prop].selector);
-          const propValue = propNode[props[prop].propName];
-          obj[prop] = propValue;
+      for (const selector of Object.keys(options.selectors)) {
+        const nodeList = [
+          ...document.querySelectorAll(options.selectors[selector].parent),
+        ];
+        //   return options.selectors[selector].parent;
+        const result = [];
+        for (const node of nodeList) {
+          console.log(node);
+          const obj = {};
+          const props = options.selectors[selector].props;
+          for (const prop of Object.keys(props)) {
+            const propNode = node.querySelector(props[prop].selector);
+            if (propNode) {
+              const propValue = propNode[props[prop]?.propName];
+              obj[prop] = propValue;
+            } else {
+              obj[prop] = null;
+            }
+          }
+          result.push(obj);
         }
-        result.push(obj);
+        allResults.push({ [selector]: result });
       }
-      allResults.push({ [selector]: result });
+    } catch (error) {
+      console.log(error);
     }
     return allResults;
   }, options);
 
-  console.log(result);
+  // console.log(result);
 
-  await browser.close();
+  const data = JSON.stringify(result, null, 2);
+
+  fs.writeFile('C:\\Temp\\teste.json', data, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
+  // await browser.close();
 })();
